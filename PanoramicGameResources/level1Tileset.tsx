0052de27b7c6b094e93cece972241f063fb4b881
almoctar.haiz <almoctar.haiz@gmail.com>
2024-03-24 12:56:28 +0100

    gitgnore

---
+++ PanoramicGameResources/level1Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="level1Tileset" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="../PanoramicGame/app/src/main/assets/level1Tileset.png" width="384" height="256"/>
+</tileset>
